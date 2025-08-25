b8a9f137ddf4483d6434ce765880bb0cb7524ac1
joseanee <126483266+joseanee@users.noreply.github.com>
2025-08-25 00:38:30 -0300

    Update purchaseRouter.ts

--- backend/src/routes/purchaseRouter.ts
+++ backend/src/routes/purchaseRouter.ts
@@ -1,5 +1,5 @@
 import { Router } from "express";
-import { getPurchaseInfo, getPurchases, newPurchase, addPayment, getTransactions, getStock, removePurchase } from "../controllers/purchaseController";
+import { getPurchaseInfo, getPurchases, newPurchase, addPayment, getTransactions, getStock, removePurchase, markPurchaseAsCompleted } from "../controllers/purchaseController";
 
 const purchaseRouter = Router();
 
@@ -10,5 +10,6 @@ purchaseRouter.get('/clients/purchases/:id', getPurchaseInfo);
 purchaseRouter.get('/clients/:id/transactions', getTransactions);
 purchaseRouter.get('/relatorios', getStock);
 purchaseRouter.delete('/purchases/:id/delete', removePurchase);
+purchaseRouter.put('/purchases/:id/complete', markPurchaseAsCompleted);
 
 export default purchaseRouter;
