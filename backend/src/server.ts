02f56f015a7c2c8b7f6b6297195745bc04b2cff7
joseanee <126483266+joseanee@users.noreply.github.com>
2026-04-09 21:01:05 -0300

    Delete backend directory

--- backend/src/server.ts
+++
@@ -1,8 +0,0 @@
-import app from "./app";
-import dotenv from "dotenv";
-
-dotenv.config();
-
-const PORT:number = Number(process.env.PORT) || 5000;
-
-app.listen(PORT, () => console.log(`Server up on port ${PORT}`));
