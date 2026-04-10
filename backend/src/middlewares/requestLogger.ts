62537d18ac942ec0254ded5e61b95bdcc2510b02
joseanee <126483266+joseanee@users.noreply.github.com>
2026-04-09 22:34:40 -0300

    Delete backend directory

--- backend/src/middlewares/requestLogger.ts
+++
@@ -1,7 +0,0 @@
-import { Request, Response, NextFunction } from "express";
-
-export default function requestLogger(req: Request, res: Response, next: NextFunction) {
-  const origin = req.headers.origin ?? req.headers.referer ?? "(no origin)";
-  console.log(`[req] ${req.method} ${req.originalUrl} origin=${origin}`);
-  next();
-}
